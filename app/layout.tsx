bdd681819dbb75bf614e6958724b365cded71d93
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-08-26 20:44:11 +0900

    feat: add github pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import GithubLink from '@/components/github-link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <GithubLink href="https://github.com/dninomiya/full-calendar-for-shadcn-ui" />
       </body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.REPOSITORY_NAME,
+};
 
 export default nextConfig;
