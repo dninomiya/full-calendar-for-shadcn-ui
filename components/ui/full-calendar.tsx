56e81d7a1652280cff3c64fe8a008d77930665ff
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2024-09-08 05:44:38 +0900

    feat(TimeTable): add current time indicator #8

--- components/ui/full-calendar.tsx
+++ components/ui/full-calendar.tsx
@@ -581,6 +581,8 @@ const CalendarCurrentDate = () => {
 };
 
 const TimeTable = () => {
+  const now = new Date();
+
   return (
     <div className="pr-2 w-12">
       {Array.from(Array(25).keys()).map((hour) => {
@@ -589,6 +591,16 @@ const TimeTable = () => {
             className="text-right relative text-xs text-muted-foreground/50 h-20 last:h-0"
             key={hour}
           >
+            {now.getHours() === hour && (
+              <div
+                className="absolute left-full translate-x-2 w-dvw h-[2px] bg-red-500"
+                style={{
+                  top: `${(now.getMinutes() / 60) * 100}%`,
+                }}
+              >
+                <div className="size-2 rounded-full bg-red-500 absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+              </div>
+            )}
             <p className="top-0 -translate-y-1/2">
               {hour === 24 ? 0 : hour}:00
             </p>
